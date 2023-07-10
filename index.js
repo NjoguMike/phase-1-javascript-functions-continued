c431f52a1f31c3f653e113bb3334d381b62625ae
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-10 11:38:20 +0300

    Complete Javascript Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(name = "*"){
+    const adj = `${name}`
+    console.log(adj)
+    debugger
+    return function (param = "special"){
+        return `You are ${adj}${param}${adj}!`
+    }
+    
+}
